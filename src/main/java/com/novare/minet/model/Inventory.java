4196b2340eec3e99beb222cefe4ee73b35cb510f
mallikaravi <mailtomallikaravi@gmail.com>
2022-09-16 17:20:50 +0200

    Update Inventory.java

--- src/main/java/com/novare/minet/model/Inventory.java
+++ src/main/java/com/novare/minet/model/Inventory.java
@@ -4,96 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Inventory {
-	private int id;
-	private Product product;
-	private double quantity;
-	private double orderedQty;
-	private List<InventoryHistory> histories = new ArrayList<>();
+	
+	List<Product> products=new ArrayList<>();
 
-	public Inventory() {
-	}
-
-	/**
-	 * @param product
-	 * @param quantity
-	 * @param orderedQty
-	 * @param histories
-	 */
-	public Inventory(Product product, double quantity, double orderedQty) {
+	public Inventory(List<Product> products) {
 		super();
-		this.product = product;
-		this.quantity = quantity;
-		this.orderedQty = orderedQty;
+		this.products = products;
 	}
 
-	/**
-	 * @return the id
-	 */
-	public int getId() {
-		return id;
+	@Override
+	public String toString() {
+		return "Inventory [products=" + products + "]";
 	}
-
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the product
-	 */
-	public Product getProduct() {
-		return product;
-	}
-
-	/**
-	 * @param product the product to set
-	 */
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-
-	/**
-	 * @return the quantity
-	 */
-	public double getQuantity() {
-		return quantity;
-	}
-
-	/**
-	 * @param quantity the quantity to set
-	 */
-	public void setQuantity(double quantity) {
-		this.quantity = quantity;
-	}
-
-	/**
-	 * @return the orderedQty
-	 */
-	public double getOrderedQty() {
-		return orderedQty;
-	}
-
-	/**
-	 * @param orderedQty the orderedQty to set
-	 */
-	public void setOrderedQty(double orderedQty) {
-		this.orderedQty = orderedQty;
-	}
-
-	/**
-	 * @return the histories
-	 */
-	public List<InventoryHistory> getHistories() {
-		return histories;
-	}
-
-	/**
-	 * @param histories the histories to set
-	 */
-	public void addHistories(InventoryHistory history) {
-		getHistories().add(history);
-	}
-
+	
+	
 }
